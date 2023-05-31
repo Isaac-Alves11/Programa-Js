fc4b063d04c4edd77948960c45f0ec6f61675561
Isaac Alves <Isaac.alves11@outlook.com>
2023-05-31 15:21:13 -0300

    Adicionei uma soma a idade, para somar a idade + 2

--- Programa.js
+++ Programa.js
@@ -1,5 +1,10 @@
 console.log("Programa Js: trabalhando com variaveis");
 
-const idade =26;
+const idade = 26;
 
-console.log("idade");
+console.log(idade);
+console.log(idade+2);
+
+const idadesomada = idade+2;
+
+console.log(idadesomada);
