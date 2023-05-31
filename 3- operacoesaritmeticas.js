8a40af70c23d2faf676f4696b7b7d71c8c2594e6
Isaac Alves <Isaac.alves11@outlook.com>
2023-05-31 16:09:22 -0300

    Criei outro arquivo para conversão de tipos

--- 3- operacoesaritmeticas.js
+++ 3- operacoesaritmeticas.js
@@ -1,2 +1,7 @@
 console.log("Operações Artiméticas");
 
+console.log(2+2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+console.log("ano" + 2020);
---
+++ 4-conversaoDetipos.js
@@ -0,0 +1,12 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt ("2"));
+
+console.log("10" / "2");
+//Nesse caso, o próprio comando força para que os número sejam divididos!
+
+console.log("Ricardo" / "2");
+//Aqui o meu comando não irá funcionar, porque, como o nome "Ricardo" não é um número, o sitema irá definir como um erro "NAN" (Not a number)!
+
