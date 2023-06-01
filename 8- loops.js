5dc627cc1a01ed35a7deb67d39b81e03af33e02c
Isaac Alves <Isaac.alves11@outlook.com>
2023-06-01 17:21:54 -0300

    incluí uma lista de destinos

---
+++ 8- loops.js
@@ -0,0 +1,27 @@
+console.log (`\nTrabalhando com condicionais`);
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+const minasGerais = `Minas Gerais`;
+
+const listaDeDestinos = new Array(
+    `salvador`,
+    `saoPaulo`,
+    `rioDeJaneiro`,
+    `minasGerais`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Salvador";
+
+console.log("Destinos possíveis");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador>= 18 || estaAcompanhada == true;
+
+let contador = 0;
+while(contador <3){
+    console.log(listaDeDestinos[contador]);
+}
