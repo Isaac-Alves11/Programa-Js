15855d8e1f1e5198f0cf1ee63fe2e4045aacda4a
Isaac Alves <Isaac.alves11@outlook.com>
2023-06-01 12:02:42 -0300

    Adicionei mais funções e comentários

--- 6- Listas.js
+++ 6- Listas.js
@@ -17,4 +17,13 @@ console.log(salvador, saoPaulo, rioDeJaneiro, minasGerais);
 console.log(listaDeDestinos);
 
 //AS teclas "CTRL + K + C" comenta um campo inteiro" e "CTRL + K + C" tira o comentário
-//Array é a maneira como a gente chama itens da lista no JavaScript
+//Array é a maneira como a gente chama itens da lista no JavaScript (O objeto Array do JavaScript é um objeto global usado na construção de 'arrays': objetos de alto nível semelhantes a listas.)
+
+listaDeDestinos.splice(1,1);
+console.log(listaDeDestinos);
+
+
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
+//Splice é um comando que eu coloco na minha lista para remover algum item que eu não quero mais nela
+//Quando estamos trabalhando com listas, a contagem não começa pelo número 1 e sim pelo número 0
+
