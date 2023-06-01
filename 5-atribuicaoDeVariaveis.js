4093b42f14f86d5582f33e48dbb436361d41499d
Isaac Alves <Isaac.alves11@outlook.com>
2023-06-01 11:32:05 -0300

    Adicioneis anotações importantes

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,12 +1,16 @@
 console.log("Trabalhando com atribuiçãom de variáveis")
 const idade = 29;
-let nome = "Ricardo";
+const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
-console.log(nome,  sobrenome);
+console.log(primeiroNome,  sobrenome);
 // também posso usar console.log(nome +" "+ sobrenome) (Concatenação)
 
-console.log (`Meu nome é ${nome} ${sobrenome}`);
+console.log (`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-nome = nome + sobrenome; 
-console.log(nome);
+let contador = 0
+contador = +1
+const nomeCompleto = primeiroNome + sobrenome; 
+console.log(primeiroNome);
+
+//É uma boa prática nomear as variáveis da melhor forma possível
